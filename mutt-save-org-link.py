2d5d64ea59449b5c7c38c6255b0bf2d0e3bace00
Stefan van der Walt <stefanv@berkeley.edu>
2018-11-02 12:09:37 -0700

    Make script identical to blog

--- mutt-save-org-link.py
+++ mutt-save-org-link.py
@@ -5,12 +5,15 @@ import email
 import subprocess
 import urllib.parse
 
+# Parse the email from standard input
 message_bytes = sys.stdin.buffer.read()
 message = email.message_from_bytes(message_bytes)
 
+# Grab the relevant message headers
 message_id = urllib.parse.quote(message['message-id'][1:-1])
 subject = message['subject']
 
+# Ask emacsclient to save a link to the message
 subprocess.Popen([
     'emacsclient',
     f'org-protocol://store-link?url=message://{message_id}&title={subject}'
